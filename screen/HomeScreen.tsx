30dae6ac35964cfcc6e73f49d2bbb3e081350a79
yorolleyhoo <obliviate219@gmail.com>
2024-07-16 16:12:33 +0900

    change UI

--- screen/HomeScreen.tsx
+++ screen/HomeScreen.tsx
@@ -8,16 +8,12 @@ import { Entypo } from '@expo/vector-icons';
 import Feather from '@expo/vector-icons/Feather';
 
 import { useState, useCallback } from 'react';
-import { storage, orderedMenuList, loadOrderedMenu} from '@/components/TachableText';
-import { limitNum } from './ProfileScreen';
-
+import { storage, orderedMenuList, loadOrderedMenu } from '@/components/TachableText';
 
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/database';
 import { firebaseConfig } from '@/database/firebase';
 import { auth } from '@/App';
-
-import { FinalAnswers } from './QuestionSheetScreen';
 
 type Navigation = NavigationProp<HomeStackList>;
 
@@ -69,8 +65,7 @@ export default function HomeScreen() {
   const navigation = useNavigation<Navigation>();
   const { width, height } = Dimensions.get('window');
   const [menuItems, setMenuItems] = useState(newMenuItems);
-  const [Drinks, setOrder] = useState(orderedMenuList);
-  const [limit, setlimit] = useState(limitNum);
+  const [Drinks, setOrder] = useState(orderedMenuList)
 
   // idで管理
   const [menuItemsId, setMenuItemsId] = useState(newMenuItemsId);
@@ -89,9 +84,10 @@ export default function HomeScreen() {
     drinkList.push(JSON.parse(item).drink)
   }
   var drinkedNum = 0;
-  if(drinkList[0] != "注文してない"){
+  var limitNum = 4;
+  if (drinkList[0] != "注文してない") {
     drinkedNum = drinkList.length;
-    if(drinkedNum >= limit){
+    if (drinkedNum >= limitNum) {
       alert("ちょっと飲みすぎじゃないですか？")
     }
   }
@@ -169,11 +165,11 @@ export default function HomeScreen() {
               </TouchableOpacity>
               </View>
             </View>
-          <TouchableOpacity 
-          style={styles.buttonResult}
-          onPress={() => {navigation.navigate('Result'); console.log(FinalAnswers)}}
-          >
-            <Image 
+            <TouchableOpacity
+              style={styles.buttonResult}
+              onPress={() => navigation.navigate('Result')}
+            >
+              <Image
                 source={require("../assets/images/bell.png")}
                 style={styles.bellimage}
               />
@@ -185,7 +181,7 @@ export default function HomeScreen() {
         </View>
         <View>
           <Text style={styles.font}>
-            {drinkedNum + "／" + limitNum + "杯目\n"}
+            {drinkedNum + "／" + limitNum + "杯目"}
           </Text>
         </View>
       <FlatList
@@ -195,6 +191,7 @@ export default function HomeScreen() {
             <Text style={styles.font}>{item}</Text>
           )}
         />
+        
         </View>
       </View>
     </ScrollView>
